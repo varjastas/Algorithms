b101099632c0d95153550a2aeda9c020225e3c09
varjastas <ctanislavkrohmal1@gmail.com>
2023-03-05 15:19:56 +0100

    Solved some new algorithms

---
+++ leetcode/delete_duplicates.py
@@ -0,0 +1,12 @@
+def removeDuplicates(nums: list[int]):
+    size = len(nums)
+    insertIndex = 1
+    for i in range(1, size):
+        # Found unique element
+        if nums[i - 1] != nums[i]:      
+            # Updating insertIndex in our main array
+            nums[insertIndex] = nums[i] 
+            # Incrementing insertIndex count by 1 
+            insertIndex = insertIndex + 1       
+    return insertIndex
+print(removeDuplicates([1, 1, 1]))
